c42359a34ee1a127dcf59729d916ac1521428f8b
Coby <cobyrpilpel2@gmail.com>
2020-12-20 01:01:02 -0500

    commit tree 675d9a710754666cbf43ef2a198a4265f8375d8f

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
-# Add your code here
+class Dog
+    
+attr_accessor :name 
+@@all = []
+
+def initialize(name)
+    @name = name
+    save
+end
+
+def self.all
+    @@all
+end
+
+def self.print_all
+
+puts @@all.map{ |dog| dog.name}
+end
+
+def self.clear_all
+    @@all.clear
+end
+
+def save 
+    @@all << self
+end
+
+end
+
+#ok. Go over this one with M. Added idea going forward. ## out methods you have already
+# tried. Maybe ## a checklist. I keep doubling back and either reusing the same code that 
+#I already know doesn't work or forgetting to put a line elsewhere to make code work and 
+# confusing myself.
